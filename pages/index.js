76f065325a95bd8a9245724726bfe7d685c17a7c
spidrteam <findmymmcrypto@gmail.com>
2022-07-25 23:31:11 +0800

    Added new routing methods

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import ArticleList from '../components/ArticleList';
 
-export default function Home() {
+export default function Home({ articles }) {
+  console.log(articles);
   return (
     <div>
       <Head>
@@ -9,6 +10,19 @@ export default function Home() {
         <meta name='keywords' content='web dev, programming'></meta>
       </Head>
       <h1>Welcome to Home Page</h1>
+
+      <ArticleList articles={articles} />
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`);
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    }
+  }
 }
